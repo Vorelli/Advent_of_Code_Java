8ab97e5fc271e125ac163a2940de083054f43056
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-06-11 02:24:43 -0400

    Finish part one of day 6!

---
+++ 6/src/main/java/dev/toscanonatale/StringDecoder.java
@@ -0,0 +1,38 @@
+package dev.toscanonatale;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class StringDecoder {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("The first argument should be the path to the file");
+            return;
+        }
+        File f = new File(args[0]);
+        System.out.println("First marker after character: " + solve(f));
+    }
+
+    public static int solve(File f) {
+        try {
+            Scanner s = new Scanner(f);
+            String line = s.nextLine();
+            for (int i = 0; i < line.length(); i++) {
+                HashSet<Character> unique = new HashSet<>();
+                for (int j = i; j < i + 4; j++) {
+                    unique.add(line.charAt(j));
+                }
+                if (unique.size() == 4) {
+                    s.close();
+                    return i + 4;
+                }
+            }
+            s.close();
+            return -1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+}
---
+++ 6/src/test/java/dev/toscanonatale/StringDecoderTest.java
@@ -0,0 +1,41 @@
+package dev.toscanonatale;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class StringDecoderTest {
+    public void testFile(String path, int expected) {
+        File f = new File(path);
+        int result = StringDecoder.solve(f.getAbsoluteFile());
+        System.out.println("Got result of " + result);
+        assertTrue(result == (expected));
+    }
+
+    @Test
+    public void testExample1() {
+        testFile("src/test/assets/test.txt", 7);
+    }
+
+    @Test
+    public void testExample2() {
+        testFile("src/test/assets/test2.txt", 5);
+    }
+
+    @Test
+    public void testExample3() {
+        testFile("src/test/assets/test3.txt", 6);
+    }
+
+    @Test
+    public void testExample4() {
+        testFile("src/test/assets/test4.txt", 10);
+    }
+
+    @Test
+    public void testExample5() {
+        testFile("src/test/assets/test5.txt", 11);
+    }
+}
